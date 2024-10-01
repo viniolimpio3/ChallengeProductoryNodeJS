23a79ec4a56617c6147de164e36096efd069d231
viniolimpio3 <viniolimpio3@gmail.com>
2024-09-30 22:34:08 -0300

    challenge productory

---
+++ readme.md
@@ -0,0 +1,5 @@
+## API para cálculo de produtório
+
+### Autores
+David Santos Pinheiro das Neves - 081210007
+Vinicius Gabriel da Silva Olimpio - 081210022
---
+++ server.js
@@ -0,0 +1,46 @@
+const app = require('./config/express')();
+const port = app.get('port');
+
+
+
+function xyz(param, param2){
+
+}
+// Routes
+app.get('/result/iterativo/:m/:n', function(request, response) {
+
+
+    let resultado = 1
+
+    let m = parseInt(request.params.m)
+    let n = parseInt(request.params.n)
+
+    for(let i = m; i <= n; i++){
+        resultado *= (i + (1 / i));
+    }
+
+    response.send({
+        resultado: parseFloat(resultado).toFixed(4)
+    });
+});
+
+app.get('/result/recursivo/:m/:n', function(request, response) {
+
+    let m = parseInt(request.params.m)
+    let n = parseInt(request.params.n)
+
+    let resultado = calcResult(m, n, 1)
+
+    response.send({
+        resultado: parseFloat(resultado).toFixed(4)
+    });
+});
+
+function calcResult(i, n, aux){
+    aux *= i + (1 / i)
+    return (i == n) ? aux : calcResult(i + 1, n, aux)
+}
+
+app.listen(port, () => {
+  console.log(`Running on localhost:${port}`)
+});
